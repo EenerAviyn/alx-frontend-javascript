6a80881c8bd9d580119369dada60dd24ebf6fc87
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2023-01-30 19:52:10 +0300

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,4 @@
-export default function updateStudentGradeByCity(students, newGrades, city) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   if (!Array.isArray(students) || !Array.isArray(newGrades)) {
     return [];
   }
